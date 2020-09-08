19c2094488d2448c7ab778eaf9171a8178786023
leanback2020 <post@leanback.no>
2020-09-08 11:03:44 +0200

    Fixed a warning

--- src/context/ProductsContext.js
+++ src/context/ProductsContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useReducer } from "react"
+import React, { createContext, useReducer } from "react"
 import axios from "axios"
 import ProductsReducer from "./ProductsReducer"
 
